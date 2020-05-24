aa297d991a2d90ec8a2c231f33779a7b50cdbc6e
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-05-24 00:50:31 -0400

    Updated to tutorial code

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 
-import productsReducer from './store/reducers/products'
+import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-  products: productsReducer
-})
+  products: productsReducer,
+  cart: cartReducer
+});
 
 const store = createStore(rootReducer);
 
@@ -18,20 +20,21 @@ const fetchFonts = () => {
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
-}
+};
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
   if (!fontLoaded) {
-    return (<AppLoading
-      startAsync={fetchFonts}
-      onFinish={() => {
-        setFontLoaded(true)
-      }}
-    />);
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
   }
-
   return (
     <Provider store={store}>
       <ShopNavigator />
--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,27 +1,29 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { Platform } from 'react-native';
 
-import ProductsOverViewScreen from '../screens/shop/ProductOverviewScreen';
-import Colors from '../constants/Colors';
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import Colors from '../constants/Colors';
 
-const ProductsNavigator = createStackNavigator({
-    ProductsOverview: ProductsOverViewScreen,
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
     ProductDetail: ProductDetailScreen
-}, {
+  },
+  {
     defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-        },
-        headerTitleStyle: {
-            fontFamily: 'open-sans-bold'
-        },
-        headerBackTitleStyle: {
-            fontFamily: 'open-sans'
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+      },
+      headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+      },
+      headerBackTitleStyle: {
+        fontFamily: 'open-sans'
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
     }
-});
+  }
+);
 
 export default createAppContainer(ProductsNavigator);
