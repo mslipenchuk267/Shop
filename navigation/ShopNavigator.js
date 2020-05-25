3ad85616210b5d4fafbd48b5cc63b4eca0a62c9f
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-05-25 18:58:43 -0400

    Added EditProductScreen to AdminNavigator

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -9,6 +9,7 @@ import Colors from '../constants/Colors';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
+import EditProductScreen from '../screens/user/EditProductScreen';
 
 const defaultNavOptions = {
   headerStyle: {
@@ -60,7 +61,8 @@ const OrdersNavigator = createStackNavigator(
 
 const AdminNavigator = createStackNavigator(
   {
-    UserProducts: UserProductsScreen
+    UserProducts: UserProductsScreen,
+    EditProduct: EditProductScreen
   },
   {
     navigationOptions: {
