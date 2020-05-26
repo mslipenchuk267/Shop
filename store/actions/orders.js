636f96a26b9af2ad2591c5166dd8dec6d030e2ee
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-05-26 15:57:38 -0400

    Added logic to store orders in DB

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,37 @@
 export const Add_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-    return { 
-        type: Add_ORDER, 
-        orderData: { items: cartItems, amount: totalAmount } 
+    return async dispatch => {
+        const date = new Date();
+        const response = await fetch(
+            'https://rn-course-17046.firebaseio.com/orders/u1.json',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cartItems,
+                    totalAmount,
+                    date: date.toISOString() // can do this on server too
+                })
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
+
+        const resData = await response.json();
+
+        dispatch({
+            type: Add_ORDER,
+            orderData: {
+                id: resData.name,
+                items: cartItems,
+                amount: totalAmount,
+                date: date
+            }
+        });
     };
 };
