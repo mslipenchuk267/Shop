ba7bddb5965fd7231296da8bfd659f2146956de1
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-05-25 20:18:27 -0400

    Now displaying an Alert when user deletes product

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { View, ScrollView, Text, TextInput, StyleSheet, Platform } from 'react-native';
+import {
+    View,
+    ScrollView,
+    Text,
+    TextInput,
+    StyleSheet,
+    Platform,
+} from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector, useDispatch } from 'react-redux';
 
