278b9d09f7a3df09d4d52abbfa329d52fed3c1a1
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-05-26 13:15:12 -0400

    Added code to handle errors related to the GET request. I display a loading spinner while the request is being made.

--- store/actions/products.js
+++ store/actions/products.js
@@ -13,22 +13,33 @@ export const fetchProducts = () => {
         // since we are returning a dispatch function,
         // execute any async code you want! becuase redux-thunk will know what to do
         // fetch returns a promise
-        const response = await fetch('https://rn-course-17046.firebaseio.com/products.json');
+        try {
+            const response = await fetch(
+                'https://rn-course-17046.firebaseio.com/products.json'
+            );
 
-        const resData = await response.json();
-        const loadedProducts = [];
-        for (const key in resData) {
-            loadedProducts.push(new Product(
-                key, 
-                'u1', 
-                resData[key].title, 
-                resData[key].imageUrl, 
-                resData[key].description, 
-                resData[key].price
-            ));
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+    
+            const resData = await response.json();
+            const loadedProducts = [];
+            for (const key in resData) {
+                loadedProducts.push(new Product(
+                    key,
+                    'u1',
+                    resData[key].title,
+                    resData[key].imageUrl,
+                    resData[key].description,
+                    resData[key].price
+                ));
+            }
+    
+            dispatch({ type: SET_PRODUCTS, products: loadedProducts })
+        } catch (err) {
+            // send to custom analytics server
+            throw err;
         }
-
-        dispatch({ type: SET_PRODUCTS, products: loadedProducts })
     }
 };
 
@@ -73,7 +84,7 @@ export const createProduct = (title, description, imageUrl, price) => {
         });
     };
 
-    
+
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
