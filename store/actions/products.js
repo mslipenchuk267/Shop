f579fe8fa6aac9a8f2eb84ba9986294361ce12f8
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-05-26 12:10:10 -0400

    Added redux-thunk and logic to asynchronously add products to the database.

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,20 +7,43 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-    return {
-        type: CREATE_PRODUCT,
-        productData: {
-            // title: title,
-            // description: description,
-            // imageUrl: imageUrl,
-            // price: price
-            // below is the same as above
-            title,
-            description,
-            imageUrl,
-            price
-        }
+    return async dispatch => {
+        // since we are returning a dispatch function,
+        // execute any async code you want! becuase redux-thunk will know what to do
+        // fetch returns a promise
+        const response = await fetch('https://rn-course-17046.firebaseio.com/products.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl,
+                price
+            })
+        });
+
+        const resData = await response.json();
+        // this only gets dispatched once the above operations are done.
+        dispatch({
+            type: CREATE_PRODUCT,
+            productData: {
+                // title: title,
+                // description: description,
+                // imageUrl: imageUrl,
+                // price: price
+                // below is the same as above
+                id: resData.name,
+                title,
+                description,
+                imageUrl,
+                price
+            }
+        });
     };
+
+    
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
