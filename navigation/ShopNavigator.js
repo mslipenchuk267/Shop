a08803ca1b806cdbaed649ba62aa39aa769bd34d
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-05-27 01:15:19 -0400

    Made basic log in screen i.e. input fields, login & signup buttons, and card style for the form

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { createStackNavigator, createDrawerNavigator, createAppContainer } from 'react-navigation';
+import { 
+  createStackNavigator, 
+  createDrawerNavigator, 
+  createAppContainer,
+  createSwitchNavigator
+} from 'react-navigation';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -10,6 +15,7 @@ import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import EditProductScreen from '../screens/user/EditProductScreen';
+import AuthScreen from '../screens/user/AuthScreen';
 
 const defaultNavOptions = {
   headerStyle: {
@@ -89,4 +95,16 @@ const ShopNavigator = createDrawerNavigator({
   }
 });
 
-export default createAppContainer(ShopNavigator);
+const AuthNavigator = createStackNavigator({
+  Auth: AuthScreen
+}, 
+{
+  defaultNavigationOptions: defaultNavOptions
+});
+
+const MainNavigator = createSwitchNavigator({
+  Auth: AuthNavigator,
+  Shop: ShopNavigator
+});
+
+export default createAppContainer(MainNavigator);
